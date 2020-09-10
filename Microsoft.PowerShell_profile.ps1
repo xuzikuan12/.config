206246263228b6b6f488468ce16e8a7a232b857d
xuzikuan12 <xuzikuan12@qq.com>
2020-09-10 16:04:08 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+# Color ls
 If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
     Import-Module Get-ChildItemColor
     
@@ -5,24 +6,34 @@ If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not i
     Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
 }
 
+# Git
 function git-status {git status}
 function git-log-12 {
 	git log --color --pretty=format:"%C(yellow)%h%Creset -%Cred%d%Creset %s %Cgreen(%ar)%Creset %C(bold blue)<%an>%Creset" -12 --graph
 }
+Set-Alias gs git-status
+Set-Alias lg git-log-12
+
+# Scoop
 function scoop-status {scoop status}
 function scoop-update {scoop update *}
+Set-Alias ss scoop-status
+Set-Alias ud scoop-update
+
+# Direction
 function work {cd D:\codes}
 function ny {cd D:\codes\Weixin\nypt_tracker}
 function page {cd D:\Codes\GitPage\xuzikuan12.github.io}
 function test {cd D:\Codes\Test}
 function jike {cd D:\Codes\Python\JiKeDownloader}
+function config {cd C:\Users\xuzik\OneDrive\Documents\.config}
+
+# C4
 function c4 {
 	cd 'D:\Program Files\bili'
 	.\c4.bat
 }
 
-Set-Alias gs git-status
-Set-Alias lg git-log-12
-Set-Alias ss scoop-status
-Set-Alias ud scoop-update
+
+
 
